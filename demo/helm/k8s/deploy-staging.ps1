b957b9b709044085cd9c5ea19f3d367d3d08b8e1
selmankoc <selman.koc@volosoft.com>
2024-05-28 11:00:43 +0300

    Update deploy script for angular-st

--- demo/helm/k8s/deploy-staging.ps1
+++ demo/helm/k8s/deploy-staging.ps1
@@ -1,3 +1,1 @@
-helm upgrade --install angular-st angular --namespace angular --create-namespace
-
-# helm upgrade --install angular-st --namespace angular --create-namespace angular -f angular/values-azure.yaml
+helm upgrade --install angular-st --namespace angular --create-namespace angular -f angular/values-azure.yaml
