6d95c41a3493ba941a19a4607853433661a50c94
selmankoc <selman.koc@volosoft.com>
2024-05-28 07:46:54 +0300

    Created demo angular helm charts

---
+++ demo/helm/k8s/deploy-staging.ps1
@@ -0,0 +1,3 @@
+helm upgrade --install angular-st angular --namespace angular --create-namespace
+
+# helm upgrade --install angular-st --namespace angular --create-namespace angular -f angular/values-azure.yaml
